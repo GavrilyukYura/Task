5ef6b1f621568844ae332da0fd8601b083b2fe97
GavrilyukYura <gavrilyuk220@gmail.com>
2022-08-26 14:14:27 +0300

    Fixed icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,11 @@
 
 const toggler = document.querySelector('.join__button');
 const content = document.querySelector('.join__menu');
+const arov = document.getElementById('arov');
+const cross = document.getElementById('cross');
 
 toggler.addEventListener('click', () => {
   content.classList.toggle('join__menu--hidden');
+  arov.classList.toggle('diseible');
+  cross.classList.toggle('diseible');
 });
